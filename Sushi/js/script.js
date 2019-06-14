d3e5f735444c2cf31e4867dbe037a32eb89875e9
mdeen1 <34013848+mdeen1@users.noreply.github.com>
2019-06-13 17:27:48 -0700

    finished header

---
+++ Sushi/js/script.js
@@ -0,0 +1,18 @@
+/*$(document).ready(function () {
+
+	 alert($('#test'));
+
+	$("#header, .info").ripples({
+	  dropRadius: 25,
+	  perturbance: 0.6,
+	  
+	});
+
+
+});*/
+
+$(document).ready(function() {
+
+
+alert(hi);
+});
